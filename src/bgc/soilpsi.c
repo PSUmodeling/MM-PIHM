1bef6c1ea8ec45f58241d5bcb396aa666924a7b4
Yuning Shi <shiyuning@gmail.com>
2017-10-06 21:52:52 -0400

    Clean up

--- src/bgc/soilpsi.c
+++ src/bgc/soilpsi.c
@@ -6,18 +6,17 @@ void SoilPsi(const soil_struct *soil, double vwc, double *psi)
      * Given a list of site constants and the soil water content,
      * this function returns the soil water potential (MPa)
      * Inputs:
-     * vwc                  (m3/m3) water mass per unit area
+     * vwc                (m3/m3) water mass per unit area
      * psi_s              (MPa)   soil matric potential
      *
      * Uses the van Genuchten relation
      */
-
     double          theta;
 
     theta = (vwc - soil->smcmin) / (soil->smcmax - soil->smcmin);
     theta = (theta > 1.0) ? 1.0 : theta;
     theta = (theta < SATMIN) ? SATMIN : theta;
 
-    /* calculate psi */
+    /* Calculate psi */
     *psi = Psi(theta, soil->alpha, soil->beta) * 1000.0 * GRAV / 1.0e6;
 }
