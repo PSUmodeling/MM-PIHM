87e7b5b2ce9bbae8231ac870787a4e00958f06cf
Yuning Shi <shiyuning@gmail.com>
2018-04-30 11:25:12 -0400

    OMP init_lc.c

--- src/init_lc.c
+++ src/init_lc.c
@@ -4,14 +4,18 @@ void InitLc(elem_struct *elem, const lctbl_struct *lctbl,
     const calib_struct *cal)
 {
     int             i;
-    int             lc_ind;
 
+#if defined(_OPENMP)
+# pragma omp parallel for
+#endif
 #if defined(_LUMPED_)
     for (i = 0; i < nelem + 1; i++)
 #else
     for (i = 0; i < nelem; i++)
 #endif
     {
+        int             lc_ind;
+
         lc_ind = elem[i].attrib.lc_type - 1;
 
         elem[i].ps.rzd = cal->rzd * lctbl->rzd[lc_ind];
@@ -32,11 +36,11 @@ void InitLc(elem_struct *elem, const lctbl_struct *lctbl,
         elem[i].lc.rough = cal->rough * lctbl->rough[lc_ind];
         elem[i].lc.cmcfactr = CMCFACTR;
         elem[i].lc.cfactr = lctbl->cfactr;
-        elem[i].lc.bare = (elem[i].attrib.lc_type == lctbl->bare) ? 1 : 0;
-        elem[i].lc.shdfac = (elem[i].lc.bare == 1) ? 0.0 : elem[i].lc.shdfac;
+        elem[i].lc.bare = (lctbl->bare == elem[i].attrib.lc_type) ? 1 : 0;
+        elem[i].lc.shdfac = (1 == elem[i].lc.bare) ? 0.0 : elem[i].lc.shdfac;
 #if defined(_NOAH_)
         elem[i].lc.snup = lctbl->snup[lc_ind];
-        elem[i].lc.isurban = (elem[i].attrib.lc_type == ISURBAN) ? 1 : 0;
+        elem[i].lc.isurban = (ISURBAN == elem[i].attrib.lc_type) ? 1 : 0;
         elem[i].lc.shdmin = 0.01;
         elem[i].lc.shdmax = 0.96;
 #endif
