4e8d4888588ccb094047d745cc23180927451a31
Yuning Shi <shiyuning@gmail.com>
2018-11-12 22:34:08 -0500

    :alembic: Swap river and river bed indexes

--- src/include/pihm_func.h
+++ src/include/pihm_func.h
@@ -46,8 +46,8 @@
 /* RT volume index */
 # define RT_UNSAT(i)        ((i) + nelem)
 # define RT_GW(i)           (i)
-# define RT_RIVER(i)        ((i) + 2 * nelem)
-# define RT_RIVBED(i)       ((i) + 2 * nelem + nriver)
+# define RT_RIVBED(i)        ((i) + 2 * nelem)
+# define RT_RIVER(i)       ((i) + 2 * nelem + nriver)
 #endif
 
 #define AvgElev(...)      _WsAreaElev(WS_ZMAX, __VA_ARGS__)
