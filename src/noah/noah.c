aac73c22ff90bc0e39fe1a05f44154d5c2ae729f
Yuning Shi <shiyuning@gmail.com>
2019-01-28 10:48:05 -0500

    :sparkles: :construction: Add glacier IcePac

--- src/noah/noah.c
+++ src/noah/noah.c
@@ -3453,48 +3453,10 @@ void SFlxGlacial(wstate_struct *ws, wflux_struct *wf, estate_struct *es,
 
     PenmanGlacial(wf, es, ef, ps, &t24, t2v, snowng, frzgra);
 
-    /*
-     * Call CanRes to calculate the canopy resistance and convert it into pc if
-     * nonzero greenness fraction
-     */
-    if (lc->shdfac > 0.0)
-    {
-#if defined(_CYCLES_)
-        CanRes(es, ps);
-#else
-        CanRes(ws, es, ef, ps, soil, epc);
-#endif
-    }
-    else
-    {
-        ps->rc = 0.0;
-    }
+    ps->rc = 0.0;
 
-    /*
-     * Now decide major pathway branch to take depending on whether snowpack
-     * exists or not
-     */
-    wf->esnow = 0.0;
-
-    if (ws->sneqv == 0.0)
-    {
-#if defined(_CYCLES_)
-        NoPac(soil, lc, cs, dt, t24, crop, ps, ws, wf, es, ef);
-#else
-        NoPac(ws, wf, es, ef, ps, lc, soil, dt, t24);
-#endif
-        ps->eta_kinematic = wf->eta * 1000.0;
-    }
-    else
-    {
-#if defined(_CYCLES_)
-        SnoPac(soil, lc, cs, snowng, dt, t24, prcpf, df1, crop, ps, ws, wf, es,
-            ef);
-#else
-        SnoPac(ws, wf, es, ef, ps, lc, soil, snowng, dt, t24, prcpf, df1);
-#endif
-        ps->eta_kinematic = (wf->esnow + wf->etns) * 1000.0;
-    }
+    IcePac(ws, wf, es, ef, ps, lc, soil, snowng, dt, t24, prcpf, df1);
+    ps->eta_kinematic = (wf->esnow + wf->etns) * 1000.0;
 
     /* Calculate effective mixing ratio at grnd level (skin) */
     ps->q1 = ps->q2 + ps->eta_kinematic * CP / ps->rch;
@@ -3626,4 +3588,270 @@ void PenmanGlacial(wflux_struct *wf, const estate_struct *es, eflux_struct *ef,
     rad = fnet / ps->rch + es->th2 - es->sfctmp;
     ps->epsca = (a * ps->rr + rad * delta) / (delta + ps->rr);
     wf->etp = ps->epsca * ps->rch / lvs / 1000.0;
+}
+
+void IcePac(wstate_struct *ws, wflux_struct *wf, estate_struct *es,
+    eflux_struct *ef, pstate_struct *ps, const lc_struct *lc,
+    const soil_struct *soil, int snowng, double dt, double t24, double prcpf,
+    double df1)
+{
+    /*
+     * Function IcePac
+     *
+     * Calculate soil moisture and heat flux values & update soil moisture
+     * content and soil heat content values for the case when a snow pack is
+     * present.
+     */
+    int             k;
+
+    double          denom;
+    double          dsoil;
+    double          dtot;
+    double          esnow1;
+    double          esnow2;
+    double          etp3;
+    double          etanrg;
+    double          ex;
+    double          seh;
+    double          sncond;
+    double          t12;
+    double          t12a;
+    double          t12b;
+    double          t14;
+    double          ssoil1;
+    double          t11;
+    double          yy;
+    double          zz1;
+    const double    ESDMIN = 1.0e-6;
+    const double    SNOEXP = 2.0;
+
+    /* Initialize evap terms. */
+    wf->dew = 0.0;
+    wf->edir = 0.0;
+    wf->ec = 0.0;
+
+    for (k = 0; k < ps->nsoil; k++)
+    {
+        wf->et[k] = 0.0;
+    }
+    wf->ett = 0.0;
+    wf->esnow = 0.0;
+    esnow1 = 0.0;
+    esnow2 = 0.0;
+
+    ps->beta = 1.0;
+
+    /* If etp < 0 (downward) then dewfall (= frostfall in this case). */
+    if (wf->etp <= 0.0)
+    {
+        if ((ps->ribb >= 0.1) && (ef->fdown > 150.0))
+        {
+            wf->etp = (((wf->etp * (1.0 - ps->ribb) < 0.0) ?
+                wf->etp * (1.0 - ps->ribb) : 0.0) / 0.980 +
+                wf->etp * (0.980 - 1.0)) / 0.980;
+        }
+
+        if (wf->etp == 0.0)
+        {
+            ps->beta = 0.0;
+        }
+
+        wf->dew = -wf->etp;
+        esnow2 = wf->etp * dt;
+        etanrg = wf->etp * 1000.0 * LSUBS;
+    }
+    else
+    {
+        wf->esnow = wf->etp;
+        esnow2 = wf->esnow * dt;
+        etanrg = wf->esnow * 1000.0 * LSUBS;
+    }
+
+    /* If precip is falling, calculate heat flux from snow sfc to newly
+     * accumulating precip.  note that this reflects the flux appropriate for
+     * the not-yet-updated skin temperature (t1).  assumes temperature of the
+     * snowfall striking the ground is = sfctmp (lowest model level air temp) */
+    ef->flx1 = 0.0;
+    if (snowng)
+    {
+        ef->flx1 = CPICE * wf->prcp * 1000.0 * (es->t1 - es->sfctmp);
+    }
+    else
+    {
+        if (wf->prcp > 0.0)
+        {
+            ef->flx1 = CPH2O * wf->prcp * 1000.0 * (es->t1 - es->sfctmp);
+        }
+    }
+
+    /* Calculate an 'effective snow-grnd sfc temp' (t12) based on heat fluxes
+     * between the snow pack and the soil and on net radiation.
+     * Include flx1 (precip-snow sfc) and flx2 (freezing rain latent heat)
+     * fluxes. flx1 from above, flx2 brought in via common block rite.
+     * flx2 reflects freezing rain latent heat flux using t1 calculated in
+     * Penman. */
+    dsoil = -0.5 * ps->zsoil[0];
+    dtot = ps->snowh + ps->iceh + dsoil;
+    denom = 1.0 + df1 / (dtot * ps->rr * ps->rch);
+
+    t12a = ((ef->fdown - ef->flx1 - ef->flx2 - ps->emissi * SIGMA * t24) /
+        ps->rch + es->th2 - es->sfctmp - etanrg / ps->rch) / ps->rr;
+    t12b = df1 * es->stc[0] / (dtot * ps->rr * ps->rch);
+
+    t12 = (es->sfctmp + t12a + t12b) / denom;
+
+    /* If the 'effective snow-grnd sfc temp' is at or below freezing, no snow
+     * melt will occur. Set the skin temp to this effective temp. Reduce (by
+     * sublimation) or increase (by frost) the depth of the snowpack, depending
+     * on sign of etp.
+     * Update soil heat flux (ssoil) using new skin temperature (t1) since no
+     * snowmelt, set accumulated snowmelt to zero, set 'effective' precip from
+     * snowmelt to zero, set phase-change heat flux from snowmelt to zero. */
+    if (t12 <= TFREEZ)
+    {
+        /* Sub-freezing block */
+        es->t1 = t12;
+        ef->ssoil = df1 * (es->t1 - es->stc[0]) / dtot;
+
+        /* Snow melts before ice */
+        if (ws->sneqv > 0.0)
+        {
+            if (ws->sneqv - esnow2 > 0.0)
+            {
+                ws->sneqv -= esnow2;
+            }
+            else
+            {
+                ws->sneqv = 0.0;
+                esnow2 = ws->sneqv;
+                wf->esnow = esnow2 / dt;
+            }
+        }
+        else
+        {
+            if (ps->iceh * ICEDENS - esnow2 > 0.0)
+            {
+                ps->iceh -= esnow2 / ICEDENS;
+            }
+            else
+            {
+                ps->iceh = 0.0;
+                esnow2 = ps->iceh * ICEDENS;
+                wf->esnow = esnow2 / dt;
+            }
+        }
+
+        ef->flx3 = 0.0;
+        ex = 0.0;
+
+        wf->snomlt = 0.0;
+    }
+    else
+    {
+        /* If the 'effective snow-grnd sfc temp' is above freezing, snow melt
+         * will occur. Call the snow melt rate, ex and amt, snomlt. Revise the
+         * effective snow depth. Revise the skin temp because it would have chgd
+         * due to the latent heat released by the melting. Calc the latent heat
+         * released, flx3.
+         * Set the effective precip, prcp1 to the snow melt rate, ex for use in
+         * SmFlx. Adjustment to t1 to account for snow patches.
+         * Calculate qsat valid at freezing point. Note that esat (saturation
+         * vapor pressure) value of 6.11e+2 used here is that valid at freezing
+         * point.
+         * Note that etp from call Penman in sflx is ignored here in favor of
+         * bulk etp over 'open water' at freezing temp. Update soil heat flux
+         * (s) using new skin temperature (t1) */
+        /* Above freezing block */
+        es->t1 = TFREEZ;
+        ps->beta = 1.0;
+        dtot = (dtot > 2.0 * dsoil) ? 2.0 * dsoil : dtot;
+        ef->ssoil = df1 * (es->t1 - es->stc[0]) / dtot;
+
+        if (ws->sneqv - esnow2 <= ESDMIN)
+        {
+            /* If potential evap (sublimation) greater than depth of snowpack.
+             * beta < 1
+             * snowpack has sublimated away, set depth to zero. */
+            ws->sneqv = 0.0;
+            ex = 0.0;
+            wf->snomlt = 0.0;
+            ef->flx3 = 0.0;
+        }
+        else
+        {
+            /* Sublimation less than depth of snowpack
+             * Snowpack (esd) reduced by esnow2 (depth of sublimated snow) */
+            ws->sneqv -= esnow2;
+            etp3 = wf->etp * 1000.0 * LVH2O;
+            seh = ps->rch * (es->t1 - es->th2);
+            t14 = es->t1 * es->t1 * es->t1 * es->t1;
+            ef->flx3 =
+                ef->fdown - ef->flx1 - ef->flx2 - ps->emissi * SIGMA * t14 -
+                ef->ssoil - seh - etanrg;
+            ef->flx3 = (ef->flx3 <= 0.0) ? 0.0 : ef->flx3;
+
+            ex = ef->flx3 * 0.001 / LSUBF;
+
+            /* Snowmelt reduction depending on snow cover */
+            wf->snomlt = ex;
+
+            /* ESDMIN represents a snowpack depth threshold value below which
+             * we choose not to retain any snowpack, and instead include it in
+             * snowmelt. */
+            if (ws->sneqv - wf->snomlt * dt >= ESDMIN)
+            {
+                ws->sneqv -= wf->snomlt * dt;
+            }
+            else
+            {
+                /* Snowmelt exceeds snow depth */
+                ex = ws->sneqv / dt;
+                ef->flx3 = ex * 1000.0 * LSUBF;
+                wf->snomlt = ws->sneqv / dt;
+
+                ws->sneqv = 0.0;
+            }
+        }
+
+        prcpf += ex;
+    }
+
+    wf->pcpdrp =  prcpf;
+
+    /* Before call ShFlx in this snowpack case, set zz1 and yy arguments to
+     * special values that ensure that ground heat flux calculated in ShFlx
+     * matches that already computer for below the snowpack, thus the sfc heat
+     * flux to be computed in ShFlx will effectively be the flux at the snow top
+     * surface.  t11 is a dummy argument so we will not use the skin temp value
+     * as revised by ShFlx. */
+    zz1 = 1.0;
+    yy = es->stc[0] - 0.5 * ef->ssoil * ps->zsoil[0] * zz1 / df1;
+
+    t11 = es->t1;
+    ssoil1 = ef->ssoil;
+
+    /* ShFlx will calc/update the soil temps. Note: the sub-sfc heat flux
+     * (ssoil1) and the skin temp (t11) output from this ShFlx call are not used
+     * in any subsequent calculations. Rather, they are dummy variables here in
+     * the SnoPac case, since the skin temp and sub-sfc heat flux are updated
+     * instead near the beginning of the call to SnoPac. */
+    ShFlx(ws, es, ef, ps, lc, soil, dt, yy, zz1, df1);
+
+    es->t1 = t11;
+    ef->ssoil = ssoil1;
+
+    /* Snow depth and density adjustment based on snow compaction. yy is assumed
+     * to be the soil temperature at the top of the soil column. */
+    if (ws->sneqv > 0.0)
+    {
+        SnowPack(ws->sneqv, dt, &ps->snowh, &ps->sndens, es->t1, yy);
+    }
+    else
+    {
+        ws->sneqv = 0.0;
+        ps->snowh = 0.0;
+        ps->sndens = 0.0;
+        sncond = 1.0;
+        ps->sncovr = 0.0;
+    }
 }
