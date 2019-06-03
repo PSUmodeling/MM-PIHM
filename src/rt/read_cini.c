d955f03a00df83002b26cc7c19e845f4e202e82e
Yuning Shi <shiyuning@gmail.com>
2019-06-03 10:50:29 -0400

    :bug: Fix bug in reading

--- src/rt/read_cini.c
+++ src/rt/read_cini.c
@@ -33,7 +33,7 @@ void ReadCini(const char filen[], const chemtbl_struct *chemtbl, int NumStc,
 
         NextLine(fp, cmdstr, &lno);
 #if defined(_FBR_)
-        match = sscanf(cmdstr, "%d %d %d %d %d %d %d %d %d %d",
+        match = sscanf(cmdstr, "%d %d %d %d %d %d",
             &index, &atttbl->prcpc[i],
             &atttbl->chem_ic[i][0], &atttbl->chem_ic[i][1],
             &atttbl->chem_ic[i][2], &atttbl->chem_ic[i][3]);
