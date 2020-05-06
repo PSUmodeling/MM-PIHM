7cd445881f5380436eb4522d4ac1b5dd6c4173a3
Yuning Shi <shiyuning@gmail.com>
2020-05-05 22:21:45 -0400

    :speech_balloon: Improve screen output

--- src/rt/react.c
+++ src/rt/react.c
@@ -552,13 +552,13 @@ void ReactControl(const chemtbl_struct chemtbl[], const kintbl_struct kintbl[],
     {
         if (roundi(substep) != roundi(stepsize))
         {
-            PIHMprintf(VL_NORMAL,
+            PIHMprintf(VL_VERBOSE,
                 " Reaction passed with minimum step of %f s.\n", substep);
         }
     }
     else
     {
-        PIHMprintf(VL_NORMAL, " Reaction failed.\n");
+        PIHMprintf(VL_VERBOSE, " Reaction failed.\n");
     }
 
     for (k = 0; k < rttbl->NumSpc; k++)
