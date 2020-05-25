c776e0528da0049674d53a624cb4acfb093202ab
Yuning Shi <shiyuning@gmail.com>
2020-05-25 10:44:24 -0400

    :rewind: Fix plot function

--- util/plot.py
+++ util/plot.py
@@ -23,41 +23,36 @@ def main():
     sim_time, gw, vname, unit = read_output('ShaleHills', 'ShaleHillsTestRun',
                                             'gw')
 
-    sim_time, unsat, vname, unit = read_output('ShaleHills', 'ShaleHillsTestRun',
-                                            'unsat')
-    #domain_xy_ratio = (np.max(x) - np.min(x)) / (np.max(y) - np.min(y))
+    domain_xy_ratio = (np.max(x) - np.min(x)) / (np.max(y) - np.min(y))
 
-    ## Average groundwater storage in time
-    #var = np.average(gw, axis=0)
+    # Average groundwater storage in time
+    var = np.average(gw, axis=0)
 
-    #fig = plt.figure(figsize=(8, 6))
-    #if domain_xy_ratio < 1.33:
-    #    ax = fig.add_axes([0, 0, 0.8, 1])
-    #else:
-    #    ax = fig.add_axes([0, 0.2, 1, 0.8])
+    fig = plt.figure(figsize=(8, 6))
+    if domain_xy_ratio < 1.33:
+        ax = fig.add_axes([0, 0, 0.8, 1])
+    else:
+        ax = fig.add_axes([0, 0.2, 1, 0.8])
 
-    ## Plot groundwater in space
-    #tpc = ax.tripcolor(x, y, tri, facecolors=var, vmin = 0, vmax = 1.5,
-    #                   edgecolors='k', lw = 0.5, cmap='RdBu')
-    #ax.set_aspect('equal')
+    # Plot groundwater in space
+    tpc = ax.tripcolor(x, y, tri, facecolors=var, vmin = 0, vmax = 1.5,
+                       edgecolors='k', lw = 0.5, cmap='RdBu')
+    ax.set_aspect('equal')
 
-    ## Add color bar with variable name and unit
-    #if domain_xy_ratio < 1.33:
-    #    cbaxes = fig.add_axes([0.8, 0.2, 0.025, 0.6])
-    #    cbar = fig.colorbar(tpc, shrink=0.8, extend='max', cax=cbaxes)
-    #else:
-    #    cbaxes = fig.add_axes([0.2, 0.15, 0.6, 0.04])
-    #    cbar = fig.colorbar(tpc, orientation='horizontal', shrink=0.8,
-    #                        extend='max', cax=cbaxes)
-    #cbar.set_label(label=vname + '(' + unit + ')', size=18)
-    #ax.axis('off')
+    # Add color bar with variable name and unit
+    if domain_xy_ratio < 1.33:
+        cbaxes = fig.add_axes([0.8, 0.2, 0.025, 0.6])
+        cbar = fig.colorbar(tpc, shrink=0.8, extend='max', cax=cbaxes)
+    else:
+        cbaxes = fig.add_axes([0.2, 0.15, 0.6, 0.04])
+        cbar = fig.colorbar(tpc, orientation='horizontal', shrink=0.8,
+                            extend='max', cax=cbaxes)
+    cbar.set_label(label=vname + '(' + unit + ')', size=18)
+    ax.axis('off')
 
     # Plot average groundwater storage in time
     fig, ax = plt.subplots(figsize=(8, 6))
-
-    totalw = gw + unsat
-
-    [ax.plot(sim_time, np.average(totalw, 1), '-o') for i in range(9)]
+    ax.plot(sim_time, np.average(gw, axis=1), '-o')
 
     # Set x and y axis labels
     ax.set_xlabel('Date', fontsize=18)
