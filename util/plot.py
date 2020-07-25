f6a4ad492db9c14eff56c4b8a50a40c4d521e082
Yuning Shi <shiyuning@gmail.com>
2020-07-24 23:03:26 -0400

    :art: Code format changes

--- util/plot.py
+++ util/plot.py
@@ -10,43 +10,60 @@ from pihm_func import read_output
 def main():
 
     '''
-    An example python script that uses functions in pihm_func to plot PIHM
-    output results
+    An example Python script that uses functions in pihm_func.py to plot MM-PIHM
+    output results.
     Assuming a ShaleHills simulation has been executed and the output directory
     is ShaleHillsTestRun inside the output directory.
     '''
 
+    # Set font size
     matplotlib.rcParams.update({'font.size': 14})
 
-    # Plot spatial distribution of groundwater
+    # Read mesh file and groundwater output. Mesh file is needed to plot spatial
+    # distribution
     _, _, tri, x, y, _, _ = read_mesh('ShaleHills')
-    sim_time, gw, vname, unit = read_output('ShaleHills', 'ShaleHillsTestRun',
-                                            'gw')
+    sim_time, gw, vname, unit = read_output('ShaleHills',           # Project
+                                            'ShaleHillsTestRun',    # Output dir
+                                            'gw')                   # Variable
 
-    domain_xy_ratio = (np.max(x) - np.min(x)) / (np.max(y) - np.min(y))
+    # Plot spatial distribution of groundwater
+    fig = plt.figure(figsize=(8, 6))
 
     # Average groundwater storage in time
     var = np.average(gw, axis=0)
 
-    fig = plt.figure(figsize=(8, 6))
+    # Test the shape of domain to determine if portrait or landscape direction
+    # should be used
+    domain_xy_ratio = (np.max(x) - np.min(x)) / (np.max(y) - np.min(y))
+
     if domain_xy_ratio < 1.33:
         ax = fig.add_axes([0, 0, 0.8, 1])
     else:
         ax = fig.add_axes([0, 0.2, 1, 0.8])
 
     # Plot groundwater in space
-    tpc = ax.tripcolor(x, y, tri, facecolors=var, vmin = 0, vmax = 1.5,
-                       edgecolors='k', lw = 0.5, cmap='RdBu')
+    tpc = ax.tripcolor(x, y, tri,
+                       facecolors=var,
+                       vmin = 0, vmax = 1.5,
+                       edgecolors='k',
+                       lw = 0.5,
+                       cmap='RdBu')
     ax.set_aspect('equal')
 
     # Add color bar with variable name and unit
     if domain_xy_ratio < 1.33:
         cbaxes = fig.add_axes([0.8, 0.2, 0.025, 0.6])
-        cbar = fig.colorbar(tpc, shrink=0.8, extend='max', cax=cbaxes)
+        cbar = fig.colorbar(tpc,
+                            shrink=0.8,
+                            extend='max',
+                            cax=cbaxes)
     else:
         cbaxes = fig.add_axes([0.2, 0.15, 0.6, 0.04])
-        cbar = fig.colorbar(tpc, orientation='horizontal', shrink=0.8,
-                            extend='max', cax=cbaxes)
+        cbar = fig.colorbar(tpc,
+                            orientation='horizontal',
+                            shrink=0.8,
+                            extend='max',
+                            cax=cbaxes)
     cbar.set_label(label=vname + '(' + unit + ')', size=18)
     ax.axis('off')
 
