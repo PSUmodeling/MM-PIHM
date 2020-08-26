df32f708434c068dbed9e61f7a334a98ec2c6014
Yuning Shi <shiyuning@gmail.com>
2020-08-26 12:25:29 -0400

    :sparkles: Warn when version check failed

--- util/check_latest_vers.sh
+++ util/check_latest_vers.sh
@@ -2,11 +2,18 @@
 
 CUR_VERS=$(grep "VERSION" src/include/pihm.h |awk '{print $3}'|tr -d '"')
 LATEST_VERS=$(curl --silent "https://api.github.com/repos/PSUmodeling/MM-PIHM/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-LATEST_VERS=${LATEST_VERS#"v"}
 
-if [ "$(printf '%s\n' $CUR_VERS $LATEST_VERS | sort -V | head -n 1)" != "$LATEST_VERS" ]; then
+if [ -z "$LATEST_VERS" ]; then
     echo
-    echo "MM-PIHM has been updated to v${LATEST_VERS}, and you are using v${CUR_VERS}."
-    echo "You can download the latest version at https://github.com/PSUmodeling/MM-PIHM/releases/tag/v${LATEST_VERS}"
+    echo "Checking latest version failed."
     echo
+else
+    LATEST_VERS=${LATEST_VERS#"v"}
+
+    if [ "$(printf '%s\n' $CUR_VERS $LATEST_VERS | sort -V | head -n 1)" != "$LATEST_VERS" ]; then
+        echo
+        echo "MM-PIHM has been updated to v${LATEST_VERS}, and you are using v${CUR_VERS}."
+        echo "You can download the latest version at https://github.com/PSUmodeling/MM-PIHM/releases/tag/v${LATEST_VERS}"
+        echo
+    fi
 fi
