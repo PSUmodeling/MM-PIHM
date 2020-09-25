0e7c6032592595b7f06418c077e7d47e91054515
Yuning Shi <shiyuning@gmail.com>
2020-09-24 23:32:16 -0400

    :sparkles: Add N in harvest output

--- src/map_output.c
+++ src/map_output.c
@@ -876,6 +876,15 @@ void MapOutput(const char outputdir[], const int prtvrbl[],
                     }
                     n++;
                     break;
+                case N_HARVEST_CTRL:
+                    InitPrintCtrl(outputdir, "n_harvest", prtvrbl[i],
+                        CN_STEP, nelem, &print->varctrl[n]);
+                    for (j = 0; j < nelem; j++)
+                    {
+                        print->varctrl[n].var[j] = &elem[j].nf.harvest;
+                    }
+                    n++;
+                    break;
                 case LAI_CTRL:
                     InitPrintCtrl(outputdir, "lai", prtvrbl[i],
                         CN_STEP, nelem, &print->varctrl[n]);
